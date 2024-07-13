51d6b4ebc70085d759e73ba57fbc5e02736bd23c
Om Moholkar <omoholkar30@gmail.com>
2024-07-14 02:34:12 +0530

    age input and display in Test

--- Test.java
+++ Test.java
@@ -9,6 +9,12 @@ class Test{
         String name = sc.next();
         int age = sc.nextInt();
         System.out.println("Hello " + name);
+        if(age >= 18){
+            System.out.println(name + " is above 18");
+        }
+        else{
+            System.out.println(name +" is below 18.");
+        }
 
     }
 }
