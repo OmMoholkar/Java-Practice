cc8c530210724aebeef081a36c46f78f563d54f3
Om Moholkar <omoholkar30@gmail.com>
2024-07-15 00:37:37 +0530

    solid Rhombus

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -27,5 +27,17 @@ class AdvancePatterns {
             }
             System.out.println();
         }
+        System.out.println();
+
+        //Solid Rhombus
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
