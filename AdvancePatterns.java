752a75bdde6338e13d48a5329b8b60fd6c70c12f
Om Moholkar <omoholkar30@gmail.com>
2024-07-15 23:28:18 +0530

    Diamond Pattern

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -66,5 +66,52 @@ class AdvancePatterns {
             }
             System.out.println();
         }
+        System.out.println();
+
+        //Diamond Pattern
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
