cee3f97a8e5aefa91cbae6e73ed7b868e9d9dd4c
Om Moholkar <omoholkar30@gmail.com>
2024-07-22 19:59:37 +0530

    Arrays Practice

--- Arrays.java
+++ Arrays.java
@@ -19,16 +19,44 @@ public class Arrays{
             System.out.println(marks[i]);
         }
 
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<size;i++){
+            if(marks[i]<min)
+                min = marks[i];
+            if(marks[i]>max) 
+                max = marks[i];
+        }
+
+        System.out.println("Maximum Marks: "+max);
+        System.out.println("Minimum Marks: "+min);
+
+        boolean isAscending = true;
+        for(int i = 0;i<size-1;i++){
+            if(marks[i]>marks[i+1]) 
+                isAscending = false;
+        }
+
+        if(isAscending) {
+            System.out.println("The array is sorted in ascending order");
+        } else {
+            System.out.println("The array is not sorted in ascending order");
+        }
+ 
+
+
         String[] names = new String[size];
         System.out.println("Enter name: ");
         for(int i=0;i<size;i++){
-            names[i]=sc.next();
+            names[i] = sc.nextLine();
         }
 
         System.out.println("Names: ");
         for(int i=0;i<size;i++){
             System.out.println(names[i]);
         }
+
+
 
 
         
