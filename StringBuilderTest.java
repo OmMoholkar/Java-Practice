8ca2e1e682be886208d904036ba1016042c2fd95
Om Moholkar <omoholkar30@gmail.com>
2024-07-24 03:50:46 +0530

    String reversal

--- StringBuilderTest.java
+++ StringBuilderTest.java
@@ -23,5 +23,25 @@ public class StringBuilderTest{
         sb.delete(3,sb.length());
         System.out.println(sb);
 
+        //String reversal 
+        StringBuilder rev = new StringBuilder("");
+        for(int i=sb.length()-1;i>=0;i--){
+            rev = rev.append(sb.charAt(i));
+        }
+        System.out.println(rev);
+
+        //string reversal 2
+        for(int i=0;i<=sb.length()/2;i++){
+            int front = i;
+            int back = sb.length()-1-i;
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+
+        System.out.println(sb);
+
     }
 }
